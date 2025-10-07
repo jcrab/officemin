1541b63a4df6ad1f987e114b2776966afbada69f
jcrab <jcrabtree@una.edu>
2025-10-07 14:25:44 -0500

    Update do_insert.php

--- do_insert.php
+++ do_insert.php
@@ -1,5 +1,7 @@
 <?php
 
+// TODO: make sure that Mallory cannot bypass the login
+
 // TODO: insert data gathered from the insert.php form into the database
 
 // after the insert send the user back to the main menu
